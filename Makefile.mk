efd1489d1f271f9a4a39c10dd3222675ea982648
Oleksiy.Tepliy <oleksiy.tepliy@developex.net>
2020-03-30 12:46:20 +0300

    makefile  refactor, replaced cmd bat with shell scripts

--- Makefile.mk
+++ Makefile.mk
@@ -1,23 +1,27 @@
+APPLICATION = HeraclesApplicationFirmware.bin
+BOOTLOADER = HeraclesBootloaderFirmware.bin
+APPLICATION_PATH = /d/__Developex__Work__/heracleskeyboardbootloader/bin/Release/HeraclesBootloaderFirmware
+BOOTLOADER_PATH = /d/__Developex__Work__/heracleskeyboardfirmware/bin/Release/HeraclesApplicationFirmware
+
+all: build load
+	@echo Done!
+
 clean:				## tidy build directory
 	@echo Tyding things up...
 	rm -f *elf *map *bin
 
-load: clean HeraclesApplicationFirmware.bin
+load:
 	JLink.exe -autoconnect 1 -device LPC54605J512 -if swd -speed 4000 -CommandFile myCmdFile.jlink
 	@echo Load Success!
 
-HeraclesApplicationFirmware.bin: build
-
 build: clean
-	cp /d/__Developex__Work__/heracleskeyboardbootloader/bin/Release/HeraclesBootloaderFirmware* .
-	cp /d/__Developex__Work__/heracleskeyboardfirmware/bin/Release/HeraclesApplicationFirmware* .
-	python ./signer.py ./HeraclesBootloaderFirmware.bin
-	python ./fwHeaderSigner.py ./HeraclesApplicationFirmware.bin 0
-	truncate -s 65536 HeraclesBootloaderFirmware.bin
-	cat HeraclesApplicationFirmware.bin >> HeraclesBootloaderFirmware.bin
-	rm -f HeraclesApplicationFirmware.bin
+	@echo Build Started!
+	cp $(APPLICATION_PATH)* .
+	cp $(BOOTLOADER_PATH)* .
+	python ./signer.py ./$(BOOTLOADER)
+	python ./fwHeaderSigner.py ./$(APPLICATION) 0
+	truncate -s 65536 $(BOOTLOADER)
+	cat $(APPLICATION) >> $(BOOTLOADER)
+	rm -f $(APPLICATION)
 	@echo Build Success!
-
-all: clean build load
-	@echo Done!
 	
